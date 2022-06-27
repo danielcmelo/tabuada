0cc8f8e9b987b3ba9d3346706d117db19685520f
Daniel Melo <dcm189497@gmail.com>
2022-06-27 22:40:56 +0100

    erro na function multiplicar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,9 @@
-alert('testando')
+    function multiplicar() {
+        let nHtml = document.getElementById('num');
+        let numero = Number(nHtml.value);
+        let tela = document.getElementById('res');
+
+        for(c = 1; c <= 10; c++){
+            console.log(c, numero)
+        }
+    }//!!!!!!erro!!!!!!!!
