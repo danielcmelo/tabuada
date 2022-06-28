cf769bbc4c592f5330e4ca658801fe11108316d9
Daniel Melo <dcm189497@gmail.com>
2022-06-28 13:55:14 +0100

    project up

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,17 @@
     function multiplicar() {
         let nHtml = document.getElementById('num');
-        let numero = Number(nHtml.value);
-        let tela = document.getElementById('res');
-
-        for(c = 1; c <= 10; c++){
-            console.log(c, numero)
+        let tela = document.getElementById('seltab');
+        if (nHtml.value.length == 0) {
+            alert('Por Favor, digite um número !');
+        } else {
+            let numero = Number(nHtml.value);
+            let c = 1;
+            tela.innerHTML = '';
+            while(c <= 10){
+                let item = document.createElement('option')
+                item.text = `${c} x ${numero} = ${c * numero}`;
+                tela.appendChild(item)
+                c++
+            }
         }
-    }//!!!!!!erro!!!!!!!!
+    }
